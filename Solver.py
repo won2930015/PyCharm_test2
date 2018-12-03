041f04168b387415bd9af1d475490289dc625d67
won2930015 <58769598@qq.com>
2018-12-04 01:01:56 +0800

    学习pycharm的列程.

--- Solver.py
+++ Solver.py
@@ -5,24 +5,28 @@ import math
 
 class Solver(object):
     # import math
-    @staticmethod
-    def calculate123():
-        a111 = int(input('a: '))
-        cdr = int(input('b: '))
-        cca1 = int(input('cca1: '))
-        d = cdr ** 2 - 4 * a111 * cca1
+    def calculate123(self):
 
+        a = int(input('a:'))
+        b = int(input('b:'))
+        c = int(input('c:'))
+        d = b ** 2 - 4 * a * c
         if d >= 0:
             disc = math.sqrt(d)
-            root3 = (-cdr + disc) / (2 * a111)
-            root2 = (-cdr - disc) / (2 * a111)
+            root3 = (-b + disc) / (2 * a)
+            root2 = (-b - disc) / (2 * a)
             print(root3, root2)
         else:
             print('error')
 
+    def aaa(self):
+        pass
+
+
+
 
 Solver().calculate123()  # 注释
-Solver.calculate123()
+# Solver.calculate123()
 
 
 
